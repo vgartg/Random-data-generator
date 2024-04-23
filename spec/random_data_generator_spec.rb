6efa9fd0480b6c81aea8b988c1b4e76417c4cc1f
vgartg <gopik539@mail.ru>
2024-04-24 00:46:07 +0300

    Добавлено большое кол-во новых методов, тестов и демонстраций

--- spec/random_data_generator_spec.rb
+++ spec/random_data_generator_spec.rb
@@ -20,7 +20,9 @@ RSpec.describe RandomDataGenerator do
       expect(number).to be <= max
     end
   end
+end
 
+RSpec.describe RandomDataGenerator do
   describe '.random_date' do
     it 'generates a random date within the specified range' do
       start_date = Date.new(2020, 1, 1)
@@ -30,9 +32,7 @@ RSpec.describe RandomDataGenerator do
       expect(date).to be <= end_date
     end
   end
-end
 
-RSpec.describe RandomDataGenerator do
   describe '.random_alpha_numeric_string' do
     it 'generates a random alpha-numeric string of specified length' do
       length = 10
@@ -46,7 +46,9 @@ RSpec.describe RandomDataGenerator do
       expect(RandomDataGenerator.random_letter_string(length)).to match(/^[a-zA-Z]{#{length}}$/)
     end
   end
+end
 
+RSpec.describe RandomDataGenerator do
   describe '.random_ip_address' do
     it 'generates a random IP address' do
       ip_address = RandomDataGenerator.random_ip_address
@@ -57,7 +59,78 @@ RSpec.describe RandomDataGenerator do
   describe '.random_email' do
     it 'generates a random email address' do
       email = RandomDataGenerator.random_email
-      expect(email).to match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
+      expect(email).to match(/^\w+[._]?\w*@[a-zA-Z_]+\.[a-zA-Z_]+(\.[a-zA-Z]{2,4}){1,2}$/)
+    end
+  end
+end
+
+RSpec.describe RandomDataGenerator do
+  describe '.random_company_name' do
+    it 'generates a random company name' do
+      company_name = RandomDataGenerator.random_company_name
+      expect(company_name).to be_a(String)
+      expect(company_name).not_to be_empty
+    end
+  end
+
+  describe '.random_phone_number' do
+    it 'generates a random phone number' do
+      phone_number = RandomDataGenerator.random_phone_number
+      expect(phone_number).to match(/^\+\d{1,3}-\d{3}-\d{3}-\d{4}$/)
+    end
+  end
+end
+
+RSpec.describe RandomDataGenerator do
+  describe '.random_person_name' do
+    it 'generates a random person name' do
+      person_name = RandomDataGenerator.random_person_name
+      expect(person_name).to be_a(String)
+      expect(person_name).not_to be_empty
+    end
+  end
+
+  describe '.random_animal_name' do
+    it 'generates a random animal name' do
+      animal_name = RandomDataGenerator.random_animal_name
+      expect(animal_name).to be_a(String)
+      expect(animal_name).not_to be_empty
+    end
+  end
+end
+
+RSpec.describe RandomDataGenerator do
+  describe '.random_text_description' do
+    it 'generates a random text description' do
+      text_description = RandomDataGenerator.random_text_description
+      expect(text_description).to be_a(String)
+      expect(text_description).not_to be_empty
+    end
+  end
+end
+
+RSpec.describe RandomDataGenerator do
+  describe '.random_url' do
+    it 'generates a random URL' do
+      url = RandomDataGenerator.random_url
+      expect(url).to match(%r{^https?://[^\s/$.?#].[^\s]*$})
+    end
+  end
+
+  describe '.random_color' do
+    it 'generates a random color' do
+      color = RandomDataGenerator.random_color
+      expect(color).to match(/^#[0-9A-F]{6}$/i)
+    end
+  end
+end
+
+RSpec.describe RandomDataGenerator do
+  describe '.random_coordinates' do
+    it 'generates random coordinates on Earth' do
+      latitude, longitude = RandomDataGenerator.random_coordinates
+      expect(latitude).to be_within(90).of(0)
+      expect(longitude).to be_within(180).of(0)
     end
   end
 end
--- src/demonstration.rb
+++ src/demonstration.rb
@@ -3,8 +3,6 @@
 require_relative '../lib/random_data_generator'
 require 'date'
 
-# Генерация и вывод случайных данных с использованием методов из модуля RandomDataGenerator
-
 puts "Random string: #{RandomDataGenerator.random_string(10)}"
 puts "Random number: #{RandomDataGenerator.random_number(1, 100)}"
 puts "Random date: #{RandomDataGenerator.random_date(Date.new(2020, 1, 1), Date.new(2025, 12, 31))}"
@@ -12,3 +10,18 @@ puts "Random alpha-numeric string: #{RandomDataGenerator.random_alpha_numeric_st
 puts "Random letter string: #{RandomDataGenerator.random_letter_string(10)}"
 puts "Random IP address: #{RandomDataGenerator.random_ip_address}"
 puts "Random email: #{RandomDataGenerator.random_email}"
+
+puts "Random company name: #{RandomDataGenerator.random_company_name}"
+puts "Random phone number: #{RandomDataGenerator.random_phone_number}"
+puts "Random person name: #{RandomDataGenerator.random_person_name}"
+puts "Random animal name: #{RandomDataGenerator.random_animal_name}"
+puts "Random text description: #{RandomDataGenerator.random_text_description}"
+puts "Random URL: #{RandomDataGenerator.random_url}"
+puts "Random color: #{RandomDataGenerator.random_color}"
+latitude, longitude = RandomDataGenerator.random_coordinates
+puts "Random coordinates: Latitude: #{latitude}, Longitude: #{longitude}"
+
+puts "Random city: #{RandomDataGenerator::RandomDataGenerator.random_city}"
+puts "Random house number: #{RandomDataGenerator::RandomDataGenerator.random_house_number}"
+puts "Random street name: #{RandomDataGenerator::RandomDataGenerator.random_street_name}"
+puts "Random address: #{RandomDataGenerator::RandomDataGenerator.random_address}"
